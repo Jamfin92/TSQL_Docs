2899c1c8cf417e1f920260d064a11c050e489419
James Finnerty <jamfin92@gmail.com>
2022-03-15 22:17:55 -0400

    tsql.min.js for highlight.js library syntax code highlighting.-Renamed StyleSheet.css to DocStyle.css, -Added highlight.js and tsql.js syntax highlighting for T-SQL <pre><code class="language-tsql"></> </> blocks, -Added T-SQL examples to Formatting on Read.cshtml View, -Next section(s) to-do: Comments, Readability. -Security (sprocs) currently being scripted.Revert "Merged previous."
    This reverts commit e4597da65d07cfac0a30ae526e1ff1845471ef0f.

--- TSQL_Docs/Controllers/HomeController.cs
+++ TSQL_Docs/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace TSQL_Docs.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About.";
 
             return View();
         }
